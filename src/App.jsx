1c4b30588de3e514964f9fa7bf6aa5a009c60bf6
snowlancer9 <topstardev.703@gmail.com>
2022-09-16 05:42:02 +0900

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import { Canvas } from "./Pages/Canvas";
-import { Television } from "./pages/Television";
 // import { AllChannels } from "./Components/AllChannels";
-// import { Television } from "./pages/Television";
+import { Television } from "./pages/Television";
 
 function App() {
   return (
