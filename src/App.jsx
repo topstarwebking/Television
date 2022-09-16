568fbd7e2988db5a77e248aae356fef63739aa17
snowlancer9 <topstardev.703@gmail.com>
2022-09-16 11:05:56 +0900

    changed component name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import Television from "./pages/Television";
+// import Television from "./pages/Television";
 // import { AllChannels } from "./Components/AllChannels";
+import Television from "./pages/PageTelevision";
 
 const App = () => {
   return (
