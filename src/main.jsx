15534b3994f62498d2d580681b18f97a8a2ff0d2
snowlancer9 <topstardev.703@gmail.com>
2022-09-16 10:55:29 +0900

    ere

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import App from "./App";
 
 import "./styles/main.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <>
+  <React.StrictMode>
     <App />
-  </>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
