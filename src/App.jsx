14cd246e05dace550fab776e301e28a53d32bbe5
snowlancer9 <topstardev.703@gmail.com>
2022-09-16 10:32:03 +0900

    sdf

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import { Television } from "./pages/Television";
 // import { AllChannels } from "./Components/AllChannels";
 
-function App() {
+const App = () => {
   return (
     <>
       <Television />
     </>
   );
-}
+};
 
 export default App;
